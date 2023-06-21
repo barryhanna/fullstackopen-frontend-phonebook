4b2c56d2ff9bf93b689ac9eb8b45067ced3db5d9
Barry Hanna <barryahanna@gmail.com>
2023-06-21 19:19:42 +0100

    Completed

--- src/App.js
+++ src/App.js
@@ -4,19 +4,37 @@ const App = () => {
 	const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
 	const [newName, setNewName] = useState('');
 
+	const handleAddPerson = (e) => {
+		e.preventDefault();
+		const newPersons = [...persons, { name: newName }];
+		setPersons(newPersons);
+	};
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form>
+			<form onSubmit={handleAddPerson}>
 				<div>
-					name: <input />
+					name:{' '}
+					<input
+						name="name"
+						type="text"
+						value={newName}
+						onChange={(e) => setNewName(e.target.value)}
+					/>
 				</div>
 				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			...
+			<div>
+				<ul style={{ listStyle: 'none' }}>
+					{persons.map((p, i) => (
+						<li key={i}>{p.name}</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
