5f0b2ab8bd98a664ce66177549d70535123a2cea
Barry Hanna <barryahanna@gmail.com>
2023-06-22 11:03:47 +0100

    Complete

--- src/App.js
+++ src/App.js
@@ -24,26 +24,58 @@ const App = () => {
 		}
 	};
 
-	const handleAddPerson = (e) => {
+	const resetInputs = () => {
+		setNewName('');
+		setNewNumber('');
+	};
+
+	const handleAddPerson = async (e) => {
 		e.preventDefault();
 		const alreadyUsed = persons.find((p) => p.name === newName);
-		if (alreadyUsed) {
-			return alert(
-				`The name ${newName} has already been added to the phonebook`
-			);
-		}
 
-		personService
-			.create({ name: newName, number: newNumber })
-			.then((data) => {
-				const newPersons = [
-					...persons,
-					{ id: data.id, name: data.name, number: data.number },
-				];
-				setPersons(newPersons);
-				setNewName('');
-				setNewNumber('');
-			});
+		if (alreadyUsed) {
+			if (alreadyUsed.number !== newNumber) {
+				if (window.confirm(`Update phone number for ${newName}?`)) {
+					const updatedPerson = await personService.update(
+						alreadyUsed.id,
+						{
+							name: alreadyUsed.name,
+							number: newNumber,
+						}
+					);
+
+					const updateIndex = persons.findIndex(
+						(person) => person.id === updatedPerson.id
+					);
+
+					const newPersons = [
+						...persons.slice(0, updateIndex),
+						updatedPerson,
+						...persons.slice(updateIndex + 1),
+					];
+
+					setPersons(newPersons);
+					resetInputs();
+					return;
+				}
+			} else {
+				alert(
+					`The name ${alreadyUsed.name} already has the number ${alreadyUsed.number}`
+				);
+				return;
+			}
+		} else {
+			personService
+				.create({ name: newName, number: newNumber })
+				.then((data) => {
+					const newPersons = [
+						...persons,
+						{ id: data.id, name: data.name, number: data.number },
+					];
+					setPersons(newPersons);
+					resetInputs();
+				});
+		}
 	};
 
 	return (
